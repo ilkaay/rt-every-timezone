ba5fa8573b42c71395ae8edd8b0855ec4c82e8b9
ilkaay <ilkaay.yuksel@gmail.com>
2021-04-20 23:42:11 +0300

    today's date has been set

--- src/components/Zone/ZoneDate.vue
+++ src/components/Zone/ZoneDate.vue
@@ -1,12 +1,32 @@
 <template>
-  <span style="font-size: 1.2rem;" :v-model="date">{{ date }}</span>
+  <span style="font-size: 1.2rem;" :v-model="date"
+    >{{ date.day }} {{ months[date.month] }} {{ date.year }}
+  </span>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      date: "15 May 1998"
+      date: {
+        day: new Date().getDate(),
+        month: new Date().getMonth(),
+        year: new Date().getFullYear()
+      },
+      months: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      ]
     };
   }
 };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import drag from "v-drag";
+import moment from "moment";
 import { store } from "./store";
 
 Vue.use(drag);
+Vue.use(moment);
 
 new Vue({
   el: "#app",
