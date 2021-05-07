9abca921acc1fcdbfab7b1816847646790bcbdb4
ilkaay <ilkaay.yuksel@gmail.com>
2021-05-07 19:17:21 +0300

    navbar and footer components created

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,29 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg p-4">
-      <img
-        src="/src/assets/time-zone.png"
-        class="border border-2 rounded-circle"
-        style="width: 3rem; height: 3rem;"
-      />
-      <span class="text-white" style="margin-left: 1rem;"
-        ><h3>Every Time Zone</h3></span
-      >
-    </nav>
+    <navbar></navbar>
     <div style="user-select: none;">
       <zones-time-line></zones-time-line>
       <current-time-line></current-time-line>
       <zone class="mt-5"></zone>
     </div>
-    <footer class="navbar p-3 mt-5">
-      <span class="text-white"><strong>İlkay Yüksel</strong></span>
-    </footer>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar/Navbar";
 import Zone from "../src/components/Zone/Zone";
-import ZonesTimeLine from "../src/components/ZonesTimeLine";
-import CurrentTimeLine from "../src/components/CurrentTimeLine";
+import ZonesTimeLine from "../src/components/TimeLine/ZonesTimeLine";
+import CurrentTimeLine from "../src/components/TimeLine/CurrentTimeLine";
+import AppFooter from "./components/Footer/Footer";
+
 export default {
   components: {
+    Navbar,
     Zone,
     ZonesTimeLine,
-    CurrentTimeLine
+    CurrentTimeLine,
+    AppFooter
   }
 };
 </script>
-
-<style scoped>
-.navbar {
-  background-color: #24273f;
-}
-</style>
---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="p-3 mt-5">
+    <span class="text-white"><strong>İlkay Yüksel</strong></span>
+  </footer>
+</template>
+
+<style scoped>
+* {
+  background-color: #24273f;
+}
+</style>
